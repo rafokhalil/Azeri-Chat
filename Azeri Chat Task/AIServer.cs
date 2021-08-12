2246a7a78b06283f3a389bb4671162a7a2751486
Xeli_hx11@itstep.edu.az <Xeli_hx11@itstep.edu.az>
2021-08-12 08:34:42 +0400

    Add project files.

---
+++ Azeri Chat Task/AIServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azeri_Chat_Task
+{
+    class AIServer
+    {
+        static List<string> salam = new List<string> {
+            "Salam","Salam necesen?","Aleykum salam","Hello qardash"
+            };
+        static List<string> necesen = new List<string> {
+            "Yaxshiyam sen?","Ela , sen necesen?","Bele de","Shukur, esas sen neceesen?"
+            };
+        static List<string> neVarNeYox = new List<string> {
+            "Salamatchiliqdi","Yaxshiliq shukur","Hech, boshluq"
+            };
+        static List<string> hardasan = new List<string> {
+            "Evde hemisheki kimi, sen hardasan?","Dincelirem","Novxanida cimirem","Kurdemirdeyem","Yiyib ichmekdeyem"
+            };
+        static List<string> randomWords = new List<string> {
+            "Basha dushmedim","Hansisa sohbeti bashla","...","Bro..."
+            };
+        static public string GetResponseSalam()
+        {
+            Random random = new Random();
+            int index = random.Next(salam.Count);
+            return salam[index];
+        }
+        static public string GetResponseNecesen()
+        {
+            Random random = new Random();
+            int index = random.Next(necesen.Count);
+            return necesen[index];
+        }
+        static public string GetResponseNeVarNeYox()
+        {
+            Random random = new Random();
+            int index = random.Next(neVarNeYox.Count);
+            return neVarNeYox[index];
+        }
+        static public string GetResponseHardasan()
+        {
+            Random random = new Random();
+            int index = random.Next(hardasan.Count);
+            return hardasan[index];
+        }
+        static public string GetResponse()
+        {
+            Random random = new Random();
+            int index = random.Next(randomWords.Count);
+            return randomWords[index];
+        }
+    }
+}
---
+++ Azeri Chat Task/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Azeri_Chat_Task
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            string space = "\n";
+
+            user.HorizontalAlignment = HorizontalAlignment.Left;
+            user.Items.Add(Message_Text_Box.Text);
+            user.Items.Add(space);
+
+            Message_Text_Box.Text = Message_Text_Box.Text.ToLower();
+            if (Message_Text_Box.Text == "salam" || Message_Text_Box.Text == "Salam")
+            {
+                ai.HorizontalAlignment = HorizontalAlignment.Right;
+                ai.Items.Add(space);
+                ai.Items.Add(AIServer.GetResponseSalam());
+            }
+            else if (Message_Text_Box.Text == "necesen?" || Message_Text_Box.Text == "Necesen?")
+            {
+                ai.HorizontalAlignment = HorizontalAlignment.Right;
+                ai.Items.Add(space);
+                ai.Items.Add(AIServer.GetResponseNecesen());
+            }
+            else if (Message_Text_Box.Text == "neyneyirsen?" || Message_Text_Box.Text == "Neyneyirsen?" || Message_Text_Box.Text == "Ne edirsen?" || Message_Text_Box.Text == "ne edirsen?")
+            {
+                ai.HorizontalAlignment = HorizontalAlignment.Right;
+                ai.Items.Add(space);
+                ai.Items.Add(AIServer.GetResponseNeVarNeYox());
+            }
+            else if (Message_Text_Box.Text == "hardasan?" || Message_Text_Box.Text == "Hardasan?")
+            {
+                ai.HorizontalAlignment = HorizontalAlignment.Right;
+                ai.Items.Add(space);
+                ai.Items.Add(AIServer.GetResponseHardasan());
+            }
+            else
+            {
+                ai.HorizontalAlignment = HorizontalAlignment.Right;
+                ai.Items.Add(space);
+                ai.Items.Add(AIServer.GetResponse());
+            }
+        }
+    }
+}
